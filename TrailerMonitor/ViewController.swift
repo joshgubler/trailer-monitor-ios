c06b4099998f2a0b54413da5e51715fde1950753
Josh Gubler <josh.gubler@gmail.com>
2021-03-28 17:00:37 -0600

    Initial Commit

---
+++ TrailerMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailerMonitor
+//
+//  Created by Josh Gubler on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
